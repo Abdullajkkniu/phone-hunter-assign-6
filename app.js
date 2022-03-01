b4c182d4085ce6fbeb78bcd0141a78fa5fab4a76
MD Abdul Alim <abdulalimjkkniu@gmail.com>
2022-03-01 20:02:01 +0600

    task completed

--- app.js
+++ app.js
@@ -1,31 +1,48 @@
 const loadMobile =()=>{
-    const searchField =document.getElementById('search-input');
-    const searchText = searchField.value;
+  const searchField =document.getElementById('search-input');
+  const searchText = searchField.value;
+  const warnings = document.getElementById('warning')
+  if(searchText ==0){
+    warnings.style.display = 'block';
+  }
+  else{
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
     .then(res =>res.json())
     .then(data => displayResult(data.data))
+    warnings.style.display = 'none';
+  }
+  searchField.value = '';
+  
 }
 const displayResult =(brand)=>{
-    console.log(brand)
-    const cardShow = document.getElementById('card-show')
-    
-    for(const phone of brand){
-        const div = document.createElement('div');
-        div.classList.add('col')
-        div.innerHTML =`
-        <div class="card h-100 w-100 mb-10">
-            <img class="w-100" src="${phone.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${phone.brand}</h5>
-              <h5 class="card-title">${phone.phone_name}</h5>
-              <button onclick="loadDetails('${phone.slug}')" class="btn btn-primary">Details</button>
-            </div>
+  console.log(brand)
+  var brands = brand.slice(0,20);
+  const cardShow = document.getElementById('card-show');
+  cardShow.textContent='';
+  if(brands==false){
+    alert('no result found')
+  }
+  else{
+    brands.forEach(phone=>{
+      const div = document.createElement('div');
+      div.classList.add('col')
+      div.innerHTML =`
+      <div class="card h-100 w-100 mb-10">
+          <img class="w-100" src="${phone.image}" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">${phone.brand}</h5>
+            <h5 class="card-title">${phone.phone_name}</h5>
+            <button onclick="loadDetails('${phone.slug}')" class="btn btn-primary">Details</button>
           </div>
-        `
-        cardShow.appendChild(div)
+        </div>
+      `
+      
+      cardShow.appendChild(div)
+        }
+      )
     }
-}
+  }
 
 const loadDetails =(phoneId)=>{
 console.log(phoneId)
@@ -38,16 +55,30 @@ fetch(url)
 const displayDetails =(details)=>{
 console.log(details)
 const detailsShow = document.getElementById('details-show');
+detailsShow.textContent = '';
 const div = document.createElement('div');
-div.innerHTML = `
-<img src="${details.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${details.name}</h5>
-          <h3>Release date: ${details.releaseDate}</h3>
-          <p><span class="fw-bold">Main features:</span>Storage: ${details.mainFeatures.storage},<br> Display-size : ${details.mainFeatures.displaySize}, <br> chipSet : ${details.mainFeatures.chipSet},<br> memory : ${details.mainFeatures.memory}, <br> <span class="fw-bold">Sensor :</span> ${details.mainFeatures.sensors}</p>
+  
 
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
+
+div.innerHTML = `
+<div class="card h-100 w-100 mb-4">
+<img src="${details.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${details.name}</h5>
+
+        <h5>Release date: ${details.releaseDate? details.releaseDate:'not found'}</h5>
+
+        <p><span class="fw-bold">Main features:</span>
+
+        <br>Storage: ${details.mainFeatures.storage? details.mainFeatures.storage: 'not found' },
+
+        <br> Display-size : ${details.mainFeatures.displaySize? details.mainFeatures.displaySize: 'not found'},
+        
+        <br> chipSet : ${details.mainFeatures.chipSet? details.mainFeatures.chipSet:'not found'},<br> memory : ${details.mainFeatures.memory}, 
+        
+        <br> <span class="fw-bold">Sensor :</span> ${details.mainFeatures.sensors? details.mainFeatures.sensors:'not found'}</p>
+      </div>
+</div>
 `
 detailsShow.appendChild(div);
 }
